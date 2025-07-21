07e38f89254fc3c6ac05684eee832143dac81f24
zest <124423053+MyZest@users.noreply.github.com>
2025-07-21 06:11:24 +0000

    feat: 修改启动命令为可写

--- start-emulator.sh
+++ start-emulator.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-# Check if the .first-boot-done file exists
+# 定义 system.img 路径
+SYSTEM_IMG="/data/android.avd/system.img"
+
+# 检查文件是否存在
+if [ ! -f "$SYSTEM_IMG" ]; then
+  echo "Error: system.img not found at $SYSTEM_IMG"
+  exit 1
+fi
+
+# 检查是否首次启动
 if [ -f /data/.first-boot-done ]; then
   RAMDISK="-ramdisk /data/android.avd/ramdisk.img"
 fi
 
-# Start the emulator with the appropriate ramdisk.img
-/opt/android-sdk/emulator/emulator -avd android -nojni -netfast -writable-system -no-boot-anim -skip-adb-auth -gpu swiftshader_indirect -no-snapshot -no-metrics $RAMDISK -qemu -m ${RAM_SIZE:-4096}
+# 启动命令
+/opt/android-sdk/emulator/emulator \
+  -avd android \
+  -nojni \
+  -netfast \
+  -writable-system \
+  -no-boot-anim \
+  -skip-adb-auth \
+  -gpu swiftshader_indirect \
+  -no-snapshot \
+  -no-metrics \
+  $RAMDISK \
+  -qemu -m ${RAM_SIZE:-4096} \
+  -drive file="$SYSTEM_IMG",if=none,id=system,format=raw,readonly=off \
+  -device virtio-blk-pci,drive=system
